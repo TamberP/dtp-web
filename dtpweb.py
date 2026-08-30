76f230e3ec3bbb4b7833bf8a6a2d12629317e937
Tamber <tamber@furryhelix.co.uk>
2026-08-30 23:24:39 +0100

    Can we tell I got sick of trying to make this shit work properly?

--- dtpweb.py
+++ dtpweb.py
@@ -1,6 +1,13 @@
-#!/home/tamber/dev/dtp-web/.venv/bin/python
-from flup.server.fcgi import WSGIServer
+#!/home/tamber/dev/dtp-web/.venv/bin/python # <-- Change this to wherever your venv python is.
 import dtpweb
+from wsgiref.simple_server import make_server
 
-theapp = dtpweb.create_app()
-WSGIServer(theapp, bindAddress='/var/run/lighttpd/fastcgi-dtp.socket').run()
+if __name__ == '__main__':
+    theapp = dtpweb.create_app()
+    with make_server('127.0.0.1', 8000, theapp) as dtpd:
+        dtpd.serve_forever()
+
+# Is this stupid? Yeah. Does it have potential godawful problems? Yeah.
+# Does it work? Also yeah.
+#
+# If your setup works better with WSGI, then use that.
